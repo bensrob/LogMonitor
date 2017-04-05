37e48401787ec185ae72a289786586f27ae7425e
bensrob <benjamin.stuart.roberts@gmail.com>
2017-04-05 17:25:35 +0000

    Initial commit

---
+++ ctail.cpp
@@ -0,0 +1,44 @@
+#include "ctail.h"
+
+ctail::ctail(std::string location)
+{
+	this->loc = location;
+	this->init();
+}
+
+ctail::ctail(const ctail &in){
+	this->loc = in.loc;
+	this->init();
+}
+
+void ctail::init(){
+	this->log.open(this->loc);
+	//if( !*this->log.is_open() )        exit(1);
+	this->log.seekg(0,std::ios_base::end);
+	this->fd = inotify_init();
+       	//if( fd < 0 )    exit(2);
+	this->wd = inotify_add_watch( fd, this->loc.c_str(), IN_MODIFY );
+	//if( wd < 0 )    exit(3);
+}
+
+std::vector<std::string> ctail::watch(){
+	std::vector<std::string> returnbuffer;
+	char buffer[BUF_LEN];
+	read( this->fd, buffer, BUF_LEN );
+	//if( length < 0) exit(4);
+	this->log.clear();
+	while( !this->log.eof() )
+	{
+		std::string line;
+		getline(this->log,line);
+		if( line.length() > 0 )	returnbuffer.push_back(line);
+	}
+	return returnbuffer;
+}
+
+ctail::~ctail(){
+	inotify_rm_watch( this->fd, this->wd );
+	close( this->fd );
+	this->log.close();
+}
+
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+	#include <csignal>
+	#include <vector>
+	#include <string>
+	#include <unistd.h>
+	#include <iostream>
+	#include <thread>
+
+	#define MAIN
+#endif
